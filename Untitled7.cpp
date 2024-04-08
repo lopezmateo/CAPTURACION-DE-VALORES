a75d33b9ed04ab37fa38830a49ae5916ea999f0a
lopezmateo <163901238+lopezmateo@users.noreply.github.com>
2024-04-08 14:21:21 -0300

    Add files via upload

---
+++ Untitled7.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+int main (){
+	int n1,n2,n3,n4,m,e,b,c,a,t,q;
+	
+	cout<<" ingrese el primer numero "<<endl;
+	cin>>n1;
+	cout<<" ingrese el segundo numero "<<endl;
+	cin>>n2;
+	cout<<" ingrese el tercer numero "<<endl;
+	cin>>n3;
+	cout<<" ingrese el cuarto numero "<<endl;
+	cin>>n4;
+	cout<<"ingrese el quinto numero "<<endl;
+	cin>>a;
+	cout<<" el resultado es "<<t<<endl;
+	
+    
+    e=c/2;
+    
+    c=n1,n2;
+
+	
+	b=m/a;
+	
+	
+	m = n3,n4;
+	
+	
+	q=1;
+	
+	t=n1,n2,e,m,b;
+	
+	return 0;
+}
